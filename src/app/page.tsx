d215ec7f9d9fa35095adbee2821389b7ea5c6d9d
daniArbesu <daniel.arbesu.sanchez@gmail.com>
2023-07-24 17:08:29 +0200

    feat: add locomotive scroll to the app

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,15 @@
+'use client';
+import { useEffect } from 'react';
 import styles from './page.module.css';
+import { createLocomotive } from '@/services/locomotive';
 
 export default function Home() {
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      // Use locomotiveScroll on the app
+      const locomotiveScroll = createLocomotive();
+    }
+  }, []);
+
   return <main className={styles.main}></main>;
 }
---
+++ src/services/locomotive.ts
@@ -0,0 +1,6 @@
+import LocomotiveScroll from 'locomotive-scroll';
+
+export const createLocomotive = () => {
+  const locomotiveScroll = new LocomotiveScroll();
+  return locomotiveScroll;
+};
