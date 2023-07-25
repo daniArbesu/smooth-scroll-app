b9e6648232674bee61ca9483b340ab782e492cba
daniArbesu <daniel.arbesu.sanchez@gmail.com>
2023-07-25 18:07:26 +0200

    feat: about section animation

--- src/components/AboutSection/index.tsx
+++ src/components/AboutSection/index.tsx
@@ -1,7 +1,15 @@
+import { aboutText } from '@/constants/text';
 import styles from './style.module.css';
+import AnimatedText from '../ui/AnimatedText';
 
 const AboutSection = () => {
-  return <section className={styles.about}>AboutSection</section>;
+  return (
+    <section className={styles.about}>
+      {aboutText.map((text, index) => (
+        <AnimatedText text={text} key={index} />
+      ))}
+    </section>
+  );
 };
 
 export default AboutSection;
--- src/components/ui/AnimatedText/index.tsx
+++ src/components/ui/AnimatedText/index.tsx
@@ -1,11 +1,30 @@
 import styles from './style.module.css';
+import { useLayoutEffect, useRef } from 'react';
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
 
-type Props = {
-  children: JSX.Element;
-};
+interface Props {
+  text: string;
+}
 
-const AnimatedText: React.FC<Props> = ({ children }) => {
-  return <div>{children}</div>;
+const AnimatedText: React.FC<Props> = ({ text }) => {
+  const textRef = useRef(null);
+
+  useLayoutEffect(() => {
+    gsap.registerPlugin(ScrollTrigger);
+
+    gsap.from(textRef.current, {
+      scrollTrigger: {
+        trigger: textRef.current,
+        start: '0px bottom',
+        end: 'bottom+=400px bottom',
+        scrub: true
+      },
+      left: '-200px',
+      opacity: 0
+    });
+  }, []);
+  return <p ref={textRef}>{text}</p>;
 };
 
 export default AnimatedText;
